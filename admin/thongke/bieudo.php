70ff149222970b7336ac828187863cb90f9f8bf6
luuhoang-star <135411218+luuhoang-star@users.noreply.github.com>
2024-03-01 15:56:16 +0700

    Add files via upload

---
+++ admin/thongke/bieudo.php
@@ -0,0 +1,51 @@
+
+<div class="row2">
+    <div class="row2 font_title">
+      <h1>BIỂU ĐỒ</h1>
+    </div>
+    <div class="row2 form_content ">
+      <div
+              id="myChart" style="width:100%; width:800px; height:500px; align-items: center">
+      </div>
+
+      <script>
+        google.charts.load('current', {'packages':['corechart']});
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+
+// Set Data
+          // const data = google.visualization.arrayToDataTable([
+          //   ['Contry', 'Mhl'],
+          //   ['Italy',54.8],
+          //   ['France',48.6],
+          //   ['Spain',44.4],
+          //   ['USA',23.9],
+          //   ['Argentina',14.5]
+          // ]);
+          const data = google.visualization.arrayToDataTable([
+          ['Danh mục', 'Số lượng'],
+          <?php
+            foreach($dsthongke as $thongke){
+              extract($thongke);
+              echo "['$name', $soluong],";
+            }
+          ?>
+          ]);
+
+// Set Options
+          const options = {
+            title:'Biểu đồ số lượng sản phẩm trong danh mục',
+            is3D:true
+          };
+
+// Draw
+          const chart = new google.visualization.PieChart(document.getElementById('myChart'));
+          chart.draw(data, options);
+
+        }
+      </script>
+
+    </div>
+  </div>
+
